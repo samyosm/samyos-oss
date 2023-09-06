8dc30a787e4b7d242cc2177b5cd5b5251b9716e7
samyosm <samy.osmium@gmail.com>
2023-09-06 18:36:34 -0400

    feat: Privacy Policy

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,11 @@
 import { component$ } from "@builder.io/qwik";
+import { Link } from "@builder.io/qwik-city";
 
 export const Footer = component$(() => {
   return (
-    <footer class='p-5 mt-32'>
+    <footer class='p-5 mt-32 flex justify-between'>
       <p>© {new Date().getFullYear()} Copyright</p>
+      <Link href="/policy">Privacy Policy</Link>
     </footer>
   )
 })
